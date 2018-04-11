9abab851849affee09ea15072c28fe1e2f950f15
otherplayer <osmk@qq.com>
2018-04-11 17:22:38 +0800

    add ele demo

--- src/components/Ele.vue
+++ src/components/Ele.vue
@@ -1,29 +1,68 @@
 <template>
   <div class="ele">
-    <el-alert
-      title="成功提示的文案"
-      type="success">
-    </el-alert>
-    <el-alert
-      title="消息提示的文案"
-      type="info">
-    </el-alert>
-    <el-alert
-      title="警告提示的文案"
-      type="warning">
-    </el-alert>
-    <el-alert
-      title="错误提示的文案"
-      type="error">
-    </el-alert>
-    <el-button :plain="true" @click="open">通知打开登录</el-button>
-    <el-button :plain="true" @click="openVn">VNode</el-button>
 
-    <!-- Form -->
-    <el-button type="text" @click="dialogFormRVisible = true">注册</el-button>
-    <el-button type="text" @click="dialogFormLVisible = true">登录</el-button>
-    <register :visible="dialogFormRVisible" @dialogRegister="dialogRegister" @dialogRegisterCancel="dialogRegisterCancel"></register>
-    <login :visible="dialogFormLVisible" @dialogLogin="dialogLogin" @dialogLoginCancel="dialogLoginCancel"></login>
+
+    <div>
+      <el-alert
+        title="成功提示的文案"
+        type="success">
+      </el-alert>
+      <el-alert
+        title="消息提示的文案"
+        type="info">
+      </el-alert>
+      <el-alert
+        title="警告提示的文案"
+        type="warning">
+      </el-alert>
+      <el-alert
+        title="错误提示的文案"
+        type="error">
+      </el-alert>
+      <el-button :plain="true" @click="open">通知打开登录</el-button>
+      <el-button :plain="true" @click="openVn">VNode</el-button>
+    </div>
+
+    <div>
+      <!-- Form -->
+      <el-button type="text" @click="dialogFormRVisible = true">注册</el-button>
+      <el-button type="text" @click="dialogFormLVisible = true">登录</el-button>
+
+      <register :visible="dialogFormRVisible" @dialogRegister="dialogRegister" @dialogRegisterCancel="dialogRegisterCancel"></register>
+      <login :visible="dialogFormLVisible" @dialogLogin="dialogLogin" @dialogLoginCancel="dialogLoginCancel"></login>
+    </div>
+
+
+
+    <div>
+      <el-button
+        type="primary"
+        @click="openFullScreen"
+        v-loading.fullscreen.lock="fullscreenLoading">
+        Loading - 指令方式
+      </el-button>
+      <el-button
+        type="primary"
+        @click="openFullScreen2">
+        Loading - 服务方式
+      </el-button>
+    </div>
+
+
+    <div>
+      <el-button
+        plain
+        @click="openNotification">
+        可自动关闭
+      </el-button>
+      <el-button
+        plain
+        @click="openNotification2">
+        不会自动关闭
+      </el-button>
+    </div>
+
+
 
     {{ $route.params.id }}
   </div>
@@ -40,6 +79,7 @@
     data () {
       return {
         msg: 'Welcome to Your Vue.js App',
+        fullscreenLoading: false,
         dialogFormRVisible: false,
         dialogFormLVisible: false,
         form: {
@@ -84,22 +124,45 @@
       dialogLoginCancel () {
         this.dialogFormLVisible = false;
       },
-      open2() {
-        this.$alert('这是一段内容', '标题名称', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$message({
-              type: 'info',
-              message: `action: ${ action }`
-            });
-          }
+      openFullScreen() {
+        this.fullscreenLoading = true;
+        setTimeout(() => {
+          this.fullscreenLoading = false;
+        }, 2000);
+      },
+      openFullScreen2() {
+        const loading = this.$loading({
+          lock: true,
+          text: 'Loading',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        });
+        setTimeout(() => {
+          loading.close();
+        }, 2000);
+      },
+      openNotification() {
+        const h = this.$createElement;
+
+        this.$notify({
+          title: '标题名称',
+          message: h('i', { style: 'color: teal'}, '这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案这是提示文案')
         });
       },
-      goBack () {
-        window.history.length > 1
-          ? this.$router.go(-1)
-          : this.$router.push('/')
+
+      openNotification2() {
+        this.$notify({
+          title: '提示',
+          message: '这是一条不会自动关闭的消息',
+          duration: 0
+        });
       }
+      // ,
+      // goBack () {
+      //   window.history.length > 1
+      //     ? this.$router.go(-1)
+      //     : this.$router.push('/')
+      // }
     }
   }
 </script>
